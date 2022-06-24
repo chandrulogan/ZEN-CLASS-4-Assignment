7b6c35e4ede7106fe5c2d2ae5906143f30717b3a
chandrulogan <78639242+chandrulogan@users.noreply.github.com>
2022-06-24 20:54:18 +0530

    Using API URL and displaying the country flags

---
+++ countryflags.js
@@ -0,0 +1,16 @@
+let request = new XMLHttpRequest()
+request.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+
+request.onload = () => {
+    console.log("from onload");
+    console.log(request.response);
+    let a = JSON.parse(request.response);
+    for (let index = 0; index < a.length; index++) {
+        console.log(a[index].flag);
+    }
+};
+
+request.onerror =() => {
+    console.error("something went wrong");
+};
